3e33d842b4a5810efb1e8fde6196af9a35277f04
gbrParanhos <gbrparanhos@gmail.com>
2024-07-09 19:34:59 -0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../dist', // saída para a pasta dist na raiz do projeto
+    emptyOutDir: true,
+  },
+  server: {
+    open: 'index.html', // abrir o index.html da pasta src
+  },
+})
